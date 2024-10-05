cee7e841a937255f91e2a62ec7b6f563737a5d62
saranya <saranyanambi911@gmail.com>
2024-10-05 22:14:56 +0530

    Hero added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,16 @@
 import "./Hero.css";
 import Heroimg from "../../assests/Hero.png";
 import { Link } from 'react-router-dom';
+import Doctorimg from "../../assests/Doctor.png";
+import Drugstoreimg from "../../assests/Drugstore.png";
+import Capsuleimg from "../../assests/Capsule.png";
+import Ambulanceimg from "../../assests/Ambulance.png";
+import Hospitalimg from "../../assests/Hospital.png"
+import Searchbar from "../Searchbar/Searchbar";
+
 const Hero=()=>{
     return(
+        <div>
         <div className="hero-container">
             <div className="hero-left">
                 <p className="herofirstline">Skip the travel! Find Online Medical Centers</p>
@@ -13,6 +21,48 @@ const Hero=()=>{
             <div className="hero-right">
                 <img src={Heroimg}/>
             </div>
+        </div>
+        {/* <div class="hero-search">
+            <Searchbar/>
+            <h3>You May be Looking For</h3>
+        <div className="card-container">
+            <div className="card-img-container">
+            <div className="card">
+                <img src={Doctorimg}/>
+                <p>Doctor</p>
+            </div>
+            </div>
+
+            <div className="card-img-container">
+            <div className="card">
+                <img src={Drugstoreimg}/>
+                <p>Labs</p>
+            </div>
+            </div>
+
+            <div className="card-img-container">
+            <div className="card">
+                <img src={Hospitalimg}/>
+                <p>Hospital</p>
+            </div>
+            </div>
+
+            <div className="card-img-container">
+            <div className="card">
+                <img src={Capsuleimg}/>
+                <p>Medical Store</p>
+            </div>
+            </div>
+
+            <div className="card-img-container">
+            <div className="card">
+                <img src={Ambulanceimg}/>
+                <p>Ambulance</p>
+            </div>
+            </div>
+
+        </div> 
+        </div>*/}
         </div>
     )
 }
