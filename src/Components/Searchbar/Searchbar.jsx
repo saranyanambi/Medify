5422f39f3c4a2fc0913bbad6c6c2127db6e892ca
saranya <saranyanambi911@gmail.com>
2024-10-06 20:57:25 +0530

    Searchbar with responsive

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,16 +1,34 @@
+
 import "./Searchbar.css";
-const Searchbar=()=>{
-    return(
-        <>
+import location from "../../assests/Location marker.png";
+import searchIcon from "../../assests/searchIcon.png";
+
+const Searchbar = () => {
+    return (
+        <div className="searchbar-container">
         <div className="searchbar">
-        <form>
-            <input type="text" placeholder="State" className="input-box"/>
-            <input type="text" placeholder="City" className="input-box"/>
-            <button className="search-btn">Search</button>
-        </form>
+            <form>
+                <div className="searchbar-container">
+                    <div className="input-container">
+                        <div className="icon-input-wrapper">
+                        <img src={location} alt="Location" className="location-icon"/>
+                        <input type="text" placeholder="State" className="input-box"/>
+                        </div>
+                    </div>
+
+                    <div className="input-container">
+                    <div className="icon-input-wrapper">
+                        <img src={location} alt="Location" className="location-icon"/>
+                        <input type="text" placeholder="City" className="input-box"/>
+                        </div>
+                    </div>
+                    
+                    <button className="search-btn"><img src={searchIcon}/>Search</button>
+                </div>
+            </form>
         </div>
-        </>
-       
-    )
+        </div>
+    );
 }
+
 export default Searchbar;
