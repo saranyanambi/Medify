81ba0002f0b3d82b0a6388ed29b32e3083205971
saranya <saranyanambi911@gmail.com>
2024-10-06 20:54:12 +0530

    footertop with responsive

--- src/Components/FooterTop/FooterTop.jsx
+++ src/Components/FooterTop/FooterTop.jsx
@@ -2,17 +2,27 @@ import arrow from "../../assests/arrowFooter.png";
 import googleplay from "../../assests/google_play.png.png";
 import appstore from "../../assests/apple_store.png.png";
 import phonelayer from "../../assests/phonelayer.png";
-import phonedisplay from "../../assests/phonelayer.png"
+import phonedisplay from "../../assests/phonedisplay.png"
 import "./FooterTop.css";
 const FooterTop=()=>{
     return(
         <div>
             <div className="footer-container">
             <div className="footer-left">
-                <div><img src={phonelayer}/></div>
-                <div className="footer-image1"><img src={phonelayer}/></div>
+                {/* <div><img src={phonelayer}/></div> */}
+                <div className="phone-layer"><img src={phonelayer}/></div>
+
+                <div className="phone-layer-second"><img src={phonelayer}/></div>
+
+                <div className="phone-display"><img src={phonedisplay}/></div>
+
+                <div className="phone-display-second"><img src={phonedisplay}/></div>
                 
 
+            </div>
+            <div className="footer-right-arrow">
+            <div>
+                <img src={arrow}/>
             </div>
             <div className="footer-right">
                 <div className="footer-first">
@@ -24,17 +34,22 @@ const FooterTop=()=>{
                 <div>
                 Get the link to download the app
                 </div>
-                <image src={arrow}/>
+               
                 <form >
                     <div className="form-container">
-                        <span>+91</span>
+                        <div className="input-country">
+                        <span className="country-code">+91</span>
                         <input type="number" placeholder="Enter the number"/>
+                        </div>
                     
                     <button className="send-btn">send sms</button>
                     </div>
                 </form>
-                <button><img src={googleplay}/></button>
-                <button><img src={appstore}/></button>
+                <div className="play-btn-container">
+                <button className="play-btn">Google Play</button>
+                <button className="play-btn">App store</button>
+                </div>
+            </div>
             </div>
             </div>
         </div>
