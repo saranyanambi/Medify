4bf805db4383f3af6a6ccdfa7db9b65f858e7533
saranya <saranyanambi911@gmail.com>
2024-10-08 17:45:53 +0530

    find and booking page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Find from "./Components/Find/Find";
+import Booking from "./Components/Booking/Booking";
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
          <Route path="/" element={<Home/>}/> 
          <Route path="/Find" element={<Find/>}/> 
+         <Route path="/booking" element={<Booking/>}/>
       </Routes>
      </BrowserRouter>
     </div>
