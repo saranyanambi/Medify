0343dd4da94e629e5e4be5032d86424e42755a7c
saranya <saranyanambi911@gmail.com>
2024-10-08 17:35:48 +0530

    Booking Added

---
+++ src/Components/Booking/Booking.jsx
@@ -0,0 +1,16 @@
+import BookingCards from "../BookingCards/BookingCards";
+import Navbar from "../Navbar/Navbar";
+import Searchbooking from "../Searchbooking/Searchbooking";
+
+const Booking=()=>{
+    return(
+        <>
+        <Navbar/>
+        <Searchbooking/>
+        <BookingCards/>
+       
+        </>
+    )
+}
+
+export default Booking;
