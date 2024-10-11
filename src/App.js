dafba01d2231953924aa4c599bd332a277cde614
saranya <saranyanambi911@gmail.com>
2024-10-11 14:46:15 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Find from "./Components/Find/Find";
 import Booking from "./Components/Booking/Booking";
+import { TotalHospitalsContext } from "./Components/Context/Context";
+import { BookingContext } from "./Components/Context/Context";
 import './App.css';
 
 function App() {
+  const [bookingData,setBookingData]=useState([]);
+  const [totalHospital,setTotalHospital]=useState([]);
   return (
     <div className="App">
+      <BookingContext.Provider value={[bookingData,setBookingData]}>
+        <TotalHospitalsContext.Provider value={[totalHospital, setTotalHospital]}>
       <BrowserRouter>
       <Routes>
          <Route path="/" element={<Home/>}/> 
@@ -15,6 +21,8 @@ function App() {
          <Route path="/booking" element={<Booking/>}/>
       </Routes>
      </BrowserRouter>
+     </TotalHospitalsContext.Provider>
+     </BookingContext.Provider>
     </div>
   );
 }
