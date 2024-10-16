9e92709b7193e0f248b3af31eec62c7dda0ae5ea
saranya <saranyanambi911@gmail.com>
2024-10-16 08:16:10 +0530

    context added

--- src/Components/Booking/Booking.jsx
+++ src/Components/Booking/Booking.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import BookingCards from "../BookingCards/BookingCards";
 import Navbar from "../Navbar/Navbar";
 import Searchbooking from "../Searchbooking/Searchbooking";
+import FooterTop from "../FooterTop/FooterTop";
+import MainFooter from "../Footer/Footer";
 
 const Booking=()=>{
     const [filteredCard,setFilteredCard]=useState([]);
     return(
         <>
-        <Navbar/>
+        <Navbar isBook={true}/>
         <Searchbooking setfilteredCard={setFilteredCard}/>
         <BookingCards filteredCard={filteredCard}/>
+        <FooterTop/>
+        <MainFooter/>
        
         </>
     )
